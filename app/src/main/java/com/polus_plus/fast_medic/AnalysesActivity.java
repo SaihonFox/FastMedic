7afba0e9d8503e46fa0937da483e1364977780a1
WhiteLittleFox <WhiteLittleFox@users.noreply.github.com>
2023-04-16 01:25:19 +0500

    Initial commit

--- app/src/main/java/com/polus_plus/fast_medic/AnalysesActivity.java
+++ app/src/main/java/com/polus_plus/fast_medic/AnalysesActivity.java
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.widget.TextView;
 
 import com.polus_plus.fast_medic.Requests.APIs.OrderUser.Orders;
+import com.polus_plus.fast_medic.Requests.APIs.UserProfile.UpdateProfileGet;
+import com.polus_plus.fast_medic.Requests.APIs.UserProfile.UpdateProfileSend;
 import com.polus_plus.fast_medic.Requests.JSONPlaceHolderAPI;
 import com.polus_plus.fast_medic.Requests.RetrofitAPI;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,7 +39,65 @@ public class AnalysesActivity extends AppCompatActivity {
 		SharedPreferences settings = getSharedPreferences("data", Context.MODE_PRIVATE);
 		
 		String token = settings.getString("token", "");
-		Call<List<Orders>> ordersCall = jsonPlaceHolderAPI.getOrders(token);
+		
+		/*JSONObject toSend = new JSONObject();
+		try {
+			toSend.put("firstname", "1");
+			toSend.put("lastname", "1");
+			toSend.put("middlename", "1");
+			toSend.put("bith", 1);
+			toSend.put("pol", "Мужской");
+		} catch (Exception ignored) {}*/
+		
+		UpdateProfileSend toSend = new UpdateProfileSend();
+		toSend.setFirstname("OKI");
+		toSend.setLastname("ABOBA");
+		toSend.setLastname("BEBRA");
+		toSend.setBith("6");
+		toSend.setPol("Tractor");
+		
+		Call<UpdateProfileGet> updateProfileGetCall = jsonPlaceHolderAPI.updateProfile(token, toSend);
+		
+		updateProfileGetCall.enqueue(new Callback<UpdateProfileGet>() {
+			@Override
+			public void onResponse(Call<UpdateProfileGet> call, Response<UpdateProfileGet> response) {
+				if(!response.isSuccessful()) {
+					String array = null;
+					try {
+						array = new JSONArray(response.body()).toString();
+					} catch (JSONException e) {
+						throw new RuntimeException(e);
+					}
+					tv.setText("Code: " + response.code() + "\nMsg: " + response.message() + "\n" + array);
+					return;
+				}
+				
+				UpdateProfileGet profileGets = response.body();
+				tv.setText(profileGets.getId());
+				//profileGets.sort(Comparator.comparingInt(UpdateProfileGet::getId));
+				
+				//String content = "length: " + profileGets.size() + "\n";
+				/*for(UpdateProfileGet profileGet : profileGets) {
+					content += "ID: " + profileGet.getId() + "\n";
+					content += "Name: " + profileGet.getFirstname() + "\n";
+					content += "LastName: " + profileGet.getLastname() + "\n";
+					content += "MiddleName: " + profileGet.getMiddlename() + "\n";
+					content += "Bith: " + profileGet.getBith() + "\n";
+					content += "Pol: " + profileGet.getPol() + "\n";
+					content += "Image: " + profileGet.getImage() + "\n";
+					
+					tv.append(content);
+					content = "";
+				}*/
+			}
+			
+			@Override
+			public void onFailure(Call<UpdateProfileGet> call, Throwable t) {
+				tv.setText(t.getMessage());
+			}
+		});
+		
+		/*Call<List<Orders>> ordersCall = jsonPlaceHolderAPI.getOrders(token);
 		
 		ordersCall.enqueue(new Callback<List<Orders>>() {
 			@Override
@@ -53,6 +119,6 @@ public class AnalysesActivity extends AppCompatActivity {
 			public void onFailure(Call<List<Orders>> call, Throwable t) {
 				tv.setText(t.getMessage());
 			}
-		});
+		});*/
 	}
 }
--- app/src/main/java/com/polus_plus/fast_medic/Requests/JSONPlaceHolderAPI.java
+++ app/src/main/java/com/polus_plus/fast_medic/Requests/JSONPlaceHolderAPI.java
@@ -10,12 +10,15 @@ import com.polus_plus.fast_medic.Requests.APIs.OrderUser.Orders;
 import com.polus_plus.fast_medic.Requests.APIs.UserProfile.UpdateProfileGet;
 import com.polus_plus.fast_medic.Requests.APIs.UserProfile.UpdateProfileSend;
 
+import org.json.JSONObject;
+
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
@@ -41,8 +44,9 @@ public interface JSONPlaceHolderAPI {
 	@POST("/api/createProfile")
 	Call<List<CreateProfile>> createProfile(@Header("Authorization") String token, @Body CreateProfile profile);
 	
+	@Headers({"Accept: application/json"})
 	@PUT("/api/updateProfile")
-	Call<UpdateProfileGet> updateProfile(@Header("Authorization") String token, @Body UpdateProfileSend updateProfile);
+	Call<UpdateProfileGet> updateProfile(@Header("Authorization") String token, @Body UpdateProfileSend json);
 	
 	@POST("/api/avatar")
 	Call<List<CreateProfile>> avatar(@Header("Authorization") String token, @Body CreateProfile profile);
